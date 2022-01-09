20046df64beff5fe5354b3f80971c46fd785dceb
Sergio Tovar <sergiotovar13@gmail.com>
2022-01-09 16:12:09 -0500

    Create many people

--- myApp.js
+++ myApp.js
@@ -9,23 +9,23 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 let Person;
 
 const PersonSchema = Schema({
-    name: {
-        type: String,
-        required: [true, 'El nombre es obligatorio'],
-    },
-    age: {
-        type: Number,
-    },
-    favoriteFoods : [{
-        type: String,
-    }]
+  name: {
+    type: String,
+    required: [true, 'El nombre es obligatorio'],
+  },
+  age: {
+    type: Number,
+  },
+  favoriteFoods: [{
+    type: String,
+  }]
 });
 
-Person = model( 'Person', PersonSchema);
+Person = model('Person', PersonSchema);
 
 const createAndSavePerson = async (done) => {
   try {
-    const person = new Person({name: 'Andres', age: 25, favoriteFoods:['Burguer','Pizza']});
+    const person = new Person({ name: 'Andres', age: 25, favoriteFoods: ['Burguer', 'Pizza'] });
     await person.save();
     done(null, person);
   } catch (error) {
@@ -33,8 +33,16 @@ const createAndSavePerson = async (done) => {
   }
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+const createManyPeople = async (arrayOfPeople, done) => {
+  try {
+    arrayOfPeople = [{ name: 'Iniesta', age: 39, favoriteFoods: ['Mate', 'Pizza'] },
+    { name: 'Messi', age: 34, favoriteFoods: ['Paella', 'Pizza'] },
+    { name: 'Xavi', age: 36, favoriteFoods: ['Chorizo', 'Pizza'] }];
+    const peopleCreated = await Person.create(arrayOfPeople);
+    done(null, peopleCreated);
+  } catch (error) {
+    done(error);
+  }
 };
 
 const findPeopleByName = (personName, done) => {
